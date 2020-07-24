3a4eb7c2c78a3ca2a381530ac071d225b85684a3
wyouflf <wyouflf@qq.com>
2020-07-24 19:50:12 +0800

    update demo

--- app/src/test/java/org/xplugin/demo/app/ExampleUnitTest.java
+++ app/src/test/java/org/xplugin/demo/app/ExampleUnitTest.java
@@ -1,8 +1,8 @@
-package org.xplugin.core;
+package org.xplugin.demo.app;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
